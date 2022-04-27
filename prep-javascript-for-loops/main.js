1d251c5705fb127c05a0b2ba512084079b587daa
Tim Chiang <tychiang922@gmail.com>
2022-04-27 23:04:01 +0000

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (let i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 9; i++) {
+  console.log(i * 2);
+}
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i);
+}
